b3d92dddfeb1b50984da29cb41abdbea0b436649
abbas <abbasqamar@msn.com>
2025-09-08 09:48:04 +0500

    language table data

---
+++ languageTableData.sql
@@ -0,0 +1,18 @@
+INSERT INTO public.languages(
+	locale, messagekey, messagecontent)
+	VALUES ('en','lng.mobile' , 'Mobile');
+ 	VALUES ('en','lng.web' , 'Web');
+	VALUES ('en','lng.desktop' , 'Desktop');
+	VALUES ('en','lng.greeting' , 'Greeting Hello');	 
+
+    VALUES ('fr','lng.mobile' , 'Mobile');
+ 	VALUES ('fr','lng.web' , 'le web');
+	VALUES ('fr','lng.desktop' , 'Desktop');
+	VALUES ('fr','lng.greeting' , 'salutation bonjour');	 
+
+	VALUES ('es','lng.mobile' , 'móvil');
+ 	VALUES ('es','lng.web' , 'web');
+	VALUES ('es','lng.desktop' , 'de oficina');
+	VALUES ('es','lng.greeting' , 'saludo hola');	 
+
+ 
